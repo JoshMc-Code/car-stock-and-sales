03eb58a2777a6e0777908e73c6464c59459d2573
JoshMc-Code <josh.mccoo@hotmail.co.uk>
2023-01-19 21:54:59 +0000

    deleted unnecessary code from sample

--- run.py
+++ run.py
@@ -1,15 +1,4 @@
-from __future__ import print_function
-
-import os.path
-
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-
-
-
 from google.oauth2 import service_account
 
 SERVICE_ACCOUNT_FILE = 'keys.json'
@@ -29,15 +18,5 @@ service = build('sheets', 'v4', credentials=creds)
 sheet = service.spreadsheets()
 result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
                             range="sales!A1:G4 ,orders!A1:G2").execute()
-values = result.get('values', [])
-
-if not values:
-    print('No data found.')
-    return
-
-print('Name, Major:')
-for row in values:
-    # Print columns A and E, which correspond to indices 0 and 4.
-    print('%s, %s' % (row[0], row[4]))
-except HttpError as err:
-print(err)
+#values = result.get('values', [])
+print(result)
