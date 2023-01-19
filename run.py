95a8bbc07cc05e5ad90507c0271aca7eb21bf193
JoshMc-Code <josh.mccoo@hotmail.co.uk>
2023-01-19 21:44:17 +0000

    editted credentials and changed to creds to match sample code

--- run.py
+++ run.py
@@ -15,15 +15,15 @@ from google.oauth2 import service_account
 SERVICE_ACCOUNT_FILE = 'keys.json'
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
-credentials = None
-credentials = service_account.Credentials.from_service_account_file(
+creds = None
+creds = service_account.Credentials.from_service_account_file(
         SERVICE_ACCOUNT_FILE, scopes=SCOPES)
 
 
 # The ID of a spreadsheet.
 SAMPLE_SPREADSHEET_ID = '1o8OyfZbM1hLk01hRgANO1dghs326qD_oEqbYpF4J8tU'
   
-  
+
 service = build('sheets', 'v4', credentials=creds)
 
 # Call the Sheets API
