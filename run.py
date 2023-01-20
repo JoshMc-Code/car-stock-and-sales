41897a419f09fab8c060f4daf807440a25757908
JoshMc-Code <josh.mccoo@hotmail.co.uk>
2023-01-20 00:32:44 +0000

    added code for sales data input. the same code as used for adding stock but for sale_update. used to update spreadsheet sales B3:G3

--- run.py
+++ run.py
@@ -24,14 +24,22 @@ st_toyota = input("Add stock for Toyota: ")
 st_nissan = input("Add stock for Nissan: ")
 st_porsche = input("Add stock for Porsche: ")
 st_ford = input("Add stock for Ford: ")
-st_mitsubishi = input("Add stock for Mitsubishi: ")
+st_mitsu = input("Add stock for Mitsubishi: ")
 st_honda = input("Add stock for Honda: ")
-st_update = [[st_toyota,st_nissan,st_porsche,st_ford,st_mitsubishi,st_honda]]
+st_update = [[st_toyota,st_nissan,st_porsche,st_ford,st_mitsu,st_honda]]
 
 print(st_update)
-
 request = sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID,
                             range="sales!B2:G2", valueInputOption="USER_ENTERED", body={"values":st_update}).execute()
-response = request
 
-print(response)
+sale_toyota = input("Add sales data for Toyota: ")
+sale_nissan = input("Add sales data for Nissan: ")
+sale_porsche = input("Add sales data for Porsche: ")
+sale_ford = input("Add sales data for Ford: ")
+sale_mitsu = input("Add sales data for Mitsubishi: ")
+sale_honda = input("Add sales data for Honda: ")
+sale_update = [[sale_toyota,sale_nissan,sale_porsche,sale_ford,sale_mitsu,sale_honda]]
+
+print(sale_update)
+request = sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                            range="sales!B3:G3", valueInputOption="USER_ENTERED", body={"values":sale_update}).execute()
