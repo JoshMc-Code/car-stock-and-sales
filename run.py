569370713ee71564038803428c524f43c5e94966
JoshMc-Code <josh.mccoo@hotmail.co.uk>
2023-01-20 01:34:22 +0000

    added code to change results from lists to int

--- run.py
+++ run.py
@@ -53,23 +53,43 @@ print(eom_values)
 eom_toyota = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
                             range="sales!B4").execute()
 eom_toyota_values = eom_toyota.get('values')
+toyota_lists = eom_toyota_values
+toyota_string_version = ''.join([''.join(map(str, sublist)) for sublist in toyota_lists])
+toyota_int_version = int(toyota_string_version)
 
 eom_nissan = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
                             range="sales!C4").execute()
 eom_nissan_values = eom_nissan.get('values')
+nissan_lists = eom_nissan_values
+nissan_string_version = ''.join([''.join(map(str, sublist)) for sublist in nissan_lists])
+nissan_int_version = int(nissan_string_version)
 
 eom_porsche = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
                             range="sales!D4").execute()
 eom_porsche_values = eom_porsche.get('values')
+porsche_lists = eom_porsche_values
+porsche_string_version = ''.join([''.join(map(str, sublist)) for sublist in porsche_lists])
+porsche_int_version = int(porsche_string_version)
 
 eom_ford = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
                             range="sales!E4").execute()
 eom_ford_values = eom_ford.get('values')
+ford_lists = eom_ford_values
+ford_string_version = ''.join([''.join(map(str, sublist)) for sublist in ford_lists])
+ford_int_version = int(ford_string_version)
 
 eom_mitsu = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
                             range="sales!F4").execute()
 eom_mitsu_values = eom_mitsu.get('values')
+mitsu_lists = eom_mitsu_values
+mitsu_string_version = ''.join([''.join(map(str, sublist)) for sublist in mitsu_lists])
+mitsu_int_version = int(mitsu_string_version)
 
 eom_honda = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
                             range="sales!G4").execute()
 eom_honda_values = eom_honda.get('values')
+honda_lists = eom_honda_values
+honda_string_version = ''.join([''.join(map(str, sublist)) for sublist in honda_lists])
+honda_int_version = int(honda_string_version)
+
+print(toyota_int_version,nissan_int_version,porsche_int_version,ford_int_version,mitsu_int_version,honda_int_version)
